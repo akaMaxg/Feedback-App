158ba0974944cf9fd2778bf3fa928093ded74ee4
Max Guclu <m.guclu@live.se>
2023-06-10 01:01:01 +0200

    Adjusted Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,11 +23,10 @@ Header.defaultProps = {
     textColor: '#ff6a95',
 }
 
-
-Header.PropTypes = {
+Header.propTypes = {
     text: PropTypes.string,
     bgColor: PropTypes.string,
     textColor: PropTypes.string
-}
-
-export default Header 
+  }
+  
+  export default Header;
